e2f2ebfafdab3d2fe5f727dec2c257f525db4bbb
Ashford Makwarimba <anesumukanya@gmail.com>
2023-06-12 14:37:01 +0200

    Update models.py

--- automation_workflow/models/models.py
+++ automation_workflow/models/models.py
@@ -61,10 +61,10 @@ class PurchaseOrder(models.Model):
     @api.model
     def create(self, vals):
         # Set the date_order from the sheet if is_import is True
-#         _logger.info(vals)
         if vals.get('is_import'):
-            _logger.info(vals.get('is_import'))
             date_order = vals.get('date_order')
+            vals['date_order'] = date_order  # Set the date_order field in the vals dictionary
+
 
         order = super(PurchaseOrder, self).create(vals)
 
